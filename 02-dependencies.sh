c5d2c1ce5052e4e0809791845bfcefbe23e7db92
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2020-07-04 10:41:20 +0100

    Create 02-dependencies.sh

---
+++ 02-dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install awesome fonts-roboto rofi compton i3lock-fancy xclip qt5-style-plugins materia-gtk-theme papirus-icon-theme breeze-cursor-theme lxappearance gnome-keyring policykit-1-gnome
