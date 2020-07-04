4d9fd28d5845c1008585759f9efe94a105266be5
Paul Fowler <43384818+Xeneye@users.noreply.github.com>
2020-07-04 10:58:34 +0100

    Update 05-vivaldi.sh

--- 05-vivaldi.sh
+++ 05-vivaldi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add - && sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'  && sudo apt update && sudo apt install vivaldi-stable
+sudo apt install msttcorefonts ttf-bitstream-vera ttf-dejavu ttf-xfree86-nonfree && wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add - && sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'  && sudo apt update && sudo apt install vivaldi-stable
